451fddb69576bac4c4a27b94f8fbcccdc006c492
Akeel Alexander <alexander_akeel@hotmail.com>
2025-11-21 13:48:47 -0400

    Create and add folder object to project

---
+++ FolderExplorer/Folder.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Collections.ObjectModel;
+
+namespace FolderExplorer
+{
+    public class Folder
+    {
+        private DirectoryInfo _folder;
+        private ObservableCollection<Folder> _subFolders;
+        private ObservableCollection<FileInfo> _files;
+
+        public Folder()
+        {
+            this.FullPath = @"c:\";
+        } // New
+
+
+        public string Name
+        {
+            get
+            {
+                return this._folder.Name;
+            }
+        }
+
+
+        public string FullPath
+        {
+            get
+            {
+                return this._folder.FullName;
+            }
+
+            set
+            {
+                if (Directory.Exists(value))
+                    this._folder = new DirectoryInfo(value);
+                else
+                    throw new ArgumentException("must exist", "fullPath");
+            }
+        }
+
+        public ObservableCollection<FileInfo> Files
+        {
+            get
+            {
+                if (this._files == null)
+                {
+                    this._files = new ObservableCollection<FileInfo>();
+
+                    FileInfo[] fi = this._folder.GetFiles();
+
+                    int i;
+                    for (i = 0; i <= fi.Length - 1; i++)
+                        this._files.Add(fi[i]);
+                }
+
+                return this._files;
+            }
+        }
+
+        public ObservableCollection<Folder> SubFolders
+        {
+            get
+            {
+                if (this._subFolders == null)
+                {
+                    try
+                    {
+                        this._subFolders = new ObservableCollection<Folder>();
+
+                        DirectoryInfo[] di = this._folder.GetDirectories();
+
+                        int i;
+                        for (i = 0; i <= di.Length - 1; i++)
+                        {
+                            Folder newFolder = new Folder();
+                            newFolder.FullPath = di[i].FullName;
+                            this._subFolders.Add(newFolder);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Trace.WriteLine(ex.Message);
+                    }
+                }
+
+                return this._subFolders;
+            }
+        }
+    }
+}
